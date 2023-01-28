1eaeab2d00068189b34f3ab36d41f27ace7a3ee3
maksmolchdmitr <62752481+maksmolchdmitr@users.noreply.github.com>
2023-01-28 11:19:51 +0300

    add Main realization and new files with matrix

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+//        Complex complex = Reader.readComplex();
+//        System.out.println("Complex = "+complex);
+//        System.out.println("Trigonometric form: "+complex.trigonometricForm());
+
+//        Reader.setScanner(new Scanner(new FileInputStream("MatrixWithRealNumbers.txt")));
+//        Matrix matrix = Reader.readMatrix(Reader::readDouble);
+//        System.out.println("Matrix:\n"+matrix);
+//        System.out.println("Determinant = "+matrix.det());
+//        System.out.println("Transpose:\n"+matrix.T());
+
+//        Reader.setScanner(new Scanner(new FileInputStream("MatrixWithComplexNumbers2.txt")));
+//        Matrix matrix = Reader.readMatrix(Reader::readComplex);
+//        System.out.println("Matrix:\n"+matrix);
+//        System.out.println("Determinant = "+matrix.det());
+//        System.out.println("Transpose:\n"+matrix.T());
     }
 }
