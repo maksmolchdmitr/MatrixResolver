59a6901c3d4d9de3cbc16a83791bd3f35231bec5
maksmolchdmitr <62752481+maksmolchdmitr@users.noreply.github.com>
2023-01-29 14:55:06 +0300

    add case 3 to read two matrix

--- src/Main.java
+++ src/Main.java
@@ -4,57 +4,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-        Scanner mainScanner = new Scanner(System.in);
+        Scanner userScanner = new Scanner(System.in);
         boolean isEnd = false;
         do{
-            Reader.setScanner(mainScanner);
+            Reader.setScanner(userScanner);
             System.out.print("""
                     Print
                     0 - to exit from program
                     1 - to read Matrix
                     2 - to read Complex
+                    3 - to read two Matrix
                     Make your choice >\040""");
             switch (Reader.readInt()) {
                 case 0 -> isEnd = true;
                 case 1 -> {
-                    System.out.print("""
-                            Print
-                            0 - to read with Complex numbers
-                            1 - to read with Real numbers
-                            Make your choice >\040""");
-                    ComplexReadable numberReadable = null;
-                    do {
-                        switch (Reader.readInt()) {
-                            case 0 -> numberReadable = Reader::readComplex;
-                            case 1 ->{
-                                numberReadable = Reader::readDouble;
-                                System.out.println("Print matrix row and col and her elements:");
-                            }
-                            default -> System.out.println("Wrong answer!");
-                        }
-                    } while (numberReadable == null);
-                    System.out.print("""
-                            Print
-                            0 - to read from file
-                            1 - to read from console
-                            Make your choice >\040""");
-                    Scanner scanner = null;
-                    do {
-                        switch (Reader.readInt()) {
-                            case 0 -> {
-                                System.out.println("Print file name > ");
-                                String fileName = Reader.readString();
-                                scanner = new Scanner(new FileInputStream(fileName));
-                            }
-                            case 1 ->{
-                                scanner = mainScanner;
-                                System.out.println("Print matrix (as the row and column sizes and its elements)");
-                            }
-                            default -> System.out.println("Wrong answer!");
-                        }
-                    } while (scanner == null);
-                    Reader.setScanner(scanner);
-                    Matrix matrix = Reader.readMatrix(numberReadable);
+                    Matrix matrix = Reader.readMatrixWithUserInterface(userScanner);
                     System.out.println("Matrix:\n" + matrix);
                     System.out.println("Transpose:\n" + matrix.T());
                     System.out.println("Determinant = " + matrix.det());
@@ -65,6 +29,22 @@ public class Main {
                     Complex complex = Reader.readComplex();
                     System.out.println("Complex = " + complex);
                     System.out.println("Trigonometric form: " + complex.trigonometricForm());
+                }
+                case 3 -> {
+                    Matrix matrix1 = Reader.readMatrixWithUserInterface(userScanner);
+                    Matrix matrix2 = Reader.readMatrixWithUserInterface(userScanner);
+                    System.out.println("Matrix_1:\n" + matrix1);
+                    System.out.println("Matrix_2:\n" + matrix2);
+                    try{
+                        System.out.println("Matrix_1*Matrix_2:\n"+Matrix.mul(matrix1, matrix2));
+                    }catch (Exception e){
+                        System.out.println("There is not mul for such matrix!");
+                    }
+                    try{
+                        System.out.println("Matrix_1+Matrix_2:\n"+Matrix.sum(matrix1, matrix2));
+                    }catch (Exception e){
+                        System.out.println("There is not sum for such matrix!");
+                    }
                 }
                 default -> System.out.println("Wrong answer!");
             }
